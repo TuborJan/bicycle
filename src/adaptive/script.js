e91aa56edf20ca824cd51f64074a3396e6326361
Kalita Dmitry <115177349+TuborJan@users.noreply.github.com>
2023-01-27 11:53:10 +0300

    Delete src/adaptive directory

--- src/adaptive/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".nav__burger").click(function (event) {
-    $(".nav__burger,.header__menu").toggleClass("active");
-    $("body").toggleClass("lock");
-  });
-});
